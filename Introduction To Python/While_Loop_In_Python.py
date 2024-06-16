4f086dbb433d62895a9e29d62ff45cae0a70c32b
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-17 00:26:10 +0530

    Add files via upload

---
+++ Introduction To Python/While_Loop_In_Python.py
@@ -0,0 +1,17 @@
+i = 1
+
+"""while i <= 10 :                 #This is an example of Infinite While Loop
+    print(i)"""
+
+"""while i <= 10 :
+    print(i)                       #This is an example of Positive While Loop
+    i += 1"""
+
+
+while i > 10 :                     #This is an Example of Negative While Loop
+    print(i)
+else:
+    print("We are Out of While Loop!!")
+
+
+print("End of the Program")
