cb19d56958cfffca2ef91a7bbcdfd353bcf09c7b
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-18 00:23:40 +0530

    Add files via upload

---
+++ Intermediate Python Programming/Functions_In_Python.py
@@ -0,0 +1,7 @@
+def my_first_function():            #Creating my own function
+    print("Inside my function")
+    print("My Name is Sudhanshu Yadav")
+    print("I am a Software Automation Tester")
+
+
+my_first_function()             #Calling my Function Here
