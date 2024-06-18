f040840b01a3f88d97ad0aa4964df594357da2df
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-19 01:40:44 +0530

    Add files via upload

---
+++ Intermediate Python Programming/Max_And_Min_Functions.py
@@ -0,0 +1,10 @@
+maximum = max(6,7,1,2,9,0,12)           #Maximum Number in the Set is 12
+minimum = min(6,7,1,2,9,0,12)           #Minimum Number in the Set is 0
+print(maximum)
+print(minimum)
+
+str1 = max("Arjun","Sudhanshu","Ravi","Parth","Zaheer")
+str2 = min("Arjun","Sudhanshu","Ravi","Parth")
+
+print(str1)            #Zaheer will be printed as Max as Z is the last letter
+print(str2)            #Arjun will be printed as Min as A is the first letter
