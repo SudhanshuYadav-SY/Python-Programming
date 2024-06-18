ef4134f35228f571768114175dd9562fbe019a33
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-19 01:30:03 +0530

    Add files via upload

---
+++ Intermediate Python Programming/Collect Input From User.py
@@ -0,0 +1,10 @@
+print("Enter your name here:")
+name = input()                  #Here we are entering & storing our name and By Default its Data Type is String
+print("Welcome user "+name)
+
+
+print("Enter first random number")
+n1 = int(input())
+print("Enter second random number")
+n2 = int(input())
+print("Multiplication of 2 Numbers = "+str(int(n1)*int(n2)))
