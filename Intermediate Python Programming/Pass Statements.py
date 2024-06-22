020207b77536b845140d4291a860684857fe20b4
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-22 18:51:02 +0530

    Add files via upload

---
+++ Intermediate Python Programming/Pass Statements.py
@@ -0,0 +1,33 @@
+def sum():
+    pass
+
+
+def sub():
+    pass
+
+def product():
+    pass
+
+def divide():
+    pass
+
+
+sum()
+sub()
+product()
+divide()
+
+
+#Case 2 Using Pass Statements in For loop
+
+for i in range(1,10):
+    pass
+
+
+#Case 3 Using Pass Statement in if Block
+
+a = 6
+b = 2
+
+if a > b :
+    pass
