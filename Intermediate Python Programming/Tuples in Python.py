54499554d291fc6bcf783a4681856d392d833919
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-29 13:25:46 +0530

    Add files via upload

--- Intermediate Python Programming/Tuples in Python.py
+++ Intermediate Python Programming/Tuples in Python.py
@@ -2,5 +2,10 @@
 a = (12,5,8,7)
 print(a)
 #Case 2 Update Value of a element in a tuple
-a[1] = 9
-print(a)
+#a[1] = 9
+#print(a)
+#Case 3 find type of tuple
+print(type(a))
+#Case 4 Indexing in a tuple
+print(a[2])                 #Forward Index
+print(a[-2])                #Backward Index Because -2 + 4 = 2
