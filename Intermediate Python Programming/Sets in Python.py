2a766e481929019fec411c740ed7a6f0c29e41db
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-30 12:58:43 +0530

    Add files via upload

--- Intermediate Python Programming/Sets in Python.py
+++ Intermediate Python Programming/Sets in Python.py
@@ -3,4 +3,8 @@ print("Output of Case 1 is here")
 a = {2,7,9,1,0}
 print(a)
 #Case 2 Print Set With Index
-print(a[1])
+#print(a[1])
+#Case 3 Check Duplicate Elements
+d = {1,8,1,7,1,5}
+print("Output of Case 3 is here")
+print(d)
