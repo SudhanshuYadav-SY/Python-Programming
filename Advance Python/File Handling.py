ce4ad2d3c08a9e97faa7441fd1d497fce547f67c
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-01 19:20:15 +0530

    Add files via upload

---
+++ Advance Python/File Handling.py
@@ -0,0 +1,5 @@
+#Case 1 Write to a file
+file = open("S:\\Python Projects\\FirstPythonProject\\Advance Python\\Sample.txt","w")         #Here w stands for writing and r stands for reading and a stands for appending
+file.write("My Name is Sudhanshu Yadav\n")
+file.write("I am looking for Job of Automation QA")
+file.close()
