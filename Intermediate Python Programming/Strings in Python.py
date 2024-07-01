62d3728c18c1b6fa83ffdd4ac285a13702b983aa
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-01 12:15:14 +0530

    Add files via upload

---
+++ Intermediate Python Programming/Strings in Python.py
@@ -0,0 +1,11 @@
+first_name = "Sudhanshu"
+last_name = 'Yadav'
+location = str("Delhi")
+#Case 1 Print values of strings
+print(first_name)
+print(last_name)
+print(location)
+#Case 2 Print data type of Variables
+print(type(first_name))
+print(type(last_name))
+print(type(location))
