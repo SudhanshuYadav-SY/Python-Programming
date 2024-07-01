a4536629e4398c6ac9d33ed89e8f3accfc6abbb5
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-01 18:19:20 +0530

    Add files via upload

--- Beginner Python Programming/In And Not In Operator.py
+++ Beginner Python Programming/In And Not In Operator.py
@@ -1,7 +1,23 @@
 #Case 1 Implement IN & NOT IN with List
-colours = ['Red','Green','Blue']
+colours_list = ['Red','Green','Blue']
 print("Case 1 Output. . .")
-print('Red' in colours)
-print('Orange' in colours)
-print('Blue' not in colours)
-print('Yellow' not in colours)
+print('Red' in colours_list)
+print('Orange' in colours_list)
+print('Blue' not in colours_list)
+print('Yellow' not in colours_list)
+
+#Case 2 Implement IN & NOT IN with Tuple
+colours_tuple = ('Red','Green','Blue')
+print("Case 2 Output. . .")
+print('Red' in colours_tuple)
+print('Orange' in colours_tuple)
+print('Blue' not in colours_tuple)
+print('Yellow' not in colours_tuple)
+
+#Case 3 Implement IN & NOT IN with Sets
+colours_set = {'Red','Green','Blue'}
+print("Case 3 Output. . .")
+print('Red' in colours_set)
+print('Orange' in colours_set)
+print('Blue' not in colours_set)
+print('Yellow' not in colours_set)
