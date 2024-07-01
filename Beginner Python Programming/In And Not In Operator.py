a5cc28fb72e29b89e3567aa10657369a3974ab70
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-01 18:15:16 +0530

    Add files via upload

---
+++ Beginner Python Programming/In And Not In Operator.py
@@ -0,0 +1,7 @@
+#Case 1 Implement IN & NOT IN with List
+colours = ['Red','Green','Blue']
+print("Case 1 Output. . .")
+print('Red' in colours)
+print('Orange' in colours)
+print('Blue' not in colours)
+print('Yellow' not in colours)
