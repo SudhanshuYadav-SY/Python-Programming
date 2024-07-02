0808f0755aa500cb61cf95e5029bc1fbbd30e85c
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-02 12:27:49 +0530

    Add files via upload

---
+++ Advance Python/Classes & Objects.py
@@ -0,0 +1,13 @@
+#This is an example of Function in Python
+"""def sample_Function():
+    print("Inside Sample Function")"""
+class Car:
+    wheels = 4
+#This is an example of Method in Python
+    def start_car(self):
+        print("Car Has Started!!")
+
+obj_Car1 = Car()           #This statement is creating an object for class Car
+print("Output as Per Object 1")
+print(obj_Car1.wheels)
+print(obj_Car1.start_car())
