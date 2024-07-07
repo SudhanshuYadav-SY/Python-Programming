9d5b0b6bf67672fe3193ea903e8092243a8e472a
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-07 13:59:59 +0530

    Add files via upload

---
+++ Advance Python/calculator.py
@@ -0,0 +1,18 @@
+def Addition(a,b):
+    sum = a + b
+    print("Addition of "+str(a)+" AND "+str(b)+" = "+str(sum))
+
+
+def Subtraction(a,b):
+    sub = a - b
+    print("Subtraction of " + str(a) + " AND " + str(b) + " = " + str(sub))
+
+
+def Product(a,b):
+    product = a * b
+    print("Product of " + str(a) + " AND " + str(b) + " = " + str(product))
+
+
+def Division(a,b):
+    divide = a / b
+    print("Division of " + str(a) + " AND " + str(b) + " = " + str(divide))
