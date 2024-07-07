a1b1e0fa7e3093049654167e005754fb030059d3
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-07 14:08:22 +0530

    Add files via upload

--- Advance Python/Modules.py
+++ Advance Python/Modules.py
@@ -1,6 +1,8 @@
-from calculator import *
-
-Addition(6,4)
-Subtraction(10,8)
-Product(5,7)
-Division(10,7)
+#from calculator import *
+import  calculator
+#Lets count how many methods are there in calculator
+print(dir(calculator))
+#Addition(6,4)
+#Subtraction(10,8)
+#Product(5,7)
+#Division(10,7)
