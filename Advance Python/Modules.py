6b7ef9d39eafabaa0923da5e6176fef8f7707b5f
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-07 13:58:19 +0530

    Add files via upload

--- Advance Python/Modules.py
+++ Advance Python/Modules.py
@@ -1,10 +1,6 @@
 import calculator
 
-r1 = calculator.Addition(6,4)
-print(r1)
-r2 = calculator.Subtraction(10,8)
-print(r2)
-r3 = calculator.Product(5,7)
-print(r3)
-r4 = calculator.Division(10,7)
-print(r4)
+calculator.Addition(6,4)
+calculator.Subtraction(10,8)
+calculator.Product(5,7)
+calculator.Division(10,7)
