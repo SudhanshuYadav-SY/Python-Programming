0a9456a19c326b1f8a48b9a530d81fa17b3ca995
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-07 13:01:02 +0530

    Add files via upload

--- Advance Python/Abstraction in Python.py
+++ Advance Python/Abstraction in Python.py
@@ -2,7 +2,12 @@ from abc import ABC, abstractmethod
 
 
 #Class A is an Abstract class because it contains one or more abstract functions
-class A(ABC):                               #ABC is a predefined class in Python Library
+class A(ABC):
+
+    def __init__(self,a):           #Created Constructor for the class
+        self.a = a
+
+    #ABC is a predefined class in Python Library
     @abstractmethod
     def method_1(self):                     #Abstract Method as no function body exists
         pass
@@ -10,10 +15,10 @@ class A(ABC):                               #ABC is a predefined class in Python
     def method_2(self):                     #Abstract Method as no function body exists
         pass
 
-    def method_3(self):
-        print("Inside Method 3")
+    def method_3(self,):
+        print("Inside Method 3 and value of a =",self.a)
 
-class B(A):
+class B(A):                             #This class will automatically become an abstract class because it's not implementing all abstract methods of parent class
     def method_1(self):
         print("Inside Method 1")
 
@@ -24,7 +29,7 @@ class C(B):
 #We can not create objects for Abstract Classes
 #To Access Abstract methods you can create a child class and implement the same
 #obj = A()
-obj = C()
+obj = C(9)
 obj.method_1()
 obj.method_2()
 obj.method_3()
