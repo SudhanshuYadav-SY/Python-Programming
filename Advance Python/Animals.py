7e17431ab6448f2382a1d36e6bae047e81cceef7
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-08 12:22:50 +0530

    Add files via upload

---
+++ Advance Python/Animals.py
@@ -0,0 +1,15 @@
+
+#Created a module named animals
+
+class Animals:
+    def Cat(self):
+        print("Hello! I am Cat")
+
+    def Dog(self):
+        print("Hello! I am a Dog")
+
+    def Rabbit(self):
+        print("Hello! I am a Rabbit")
+
+    def Lion(self):
+        print("Hello! I am a Lion")
