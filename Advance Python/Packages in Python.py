0ba9b5915ac65c41a8a95aa5d6aeb8078cb1f2b3
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-08 13:16:38 +0530

    Add files via upload

--- Advance Python/Packages in Python.py
+++ Advance Python/Packages in Python.py
@@ -1,8 +1,10 @@
-from mathPackage import calculator
+from mathPackage.calculator import Addition
+from mathPackage.calculator import Subtraction
+from mathPackage.calculator import Product
+from mathPackage.calculator import Division
 
 
-
-r1 = calculator.Addition(18,17)
-r2 = calculator.Subtraction(18,17)
-r3 = calculator.Product(18,17)
-r4 = calculator.Division(18,17)
+r1 = Addition(18,17)
+r2 = Subtraction(18,17)
+r3 = Product(18,17)
+r4 = Division(18,17)
