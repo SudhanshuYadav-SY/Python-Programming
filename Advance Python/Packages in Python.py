6ec54241a9e19f983b57c55645de6b9bd809ccf3
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-08 13:06:14 +0530

    Add files via upload

---
+++ Advance Python/Packages in Python.py
@@ -0,0 +1,8 @@
+import mathPackage.calculator
+
+
+
+r1 = mathPackage.calculator.Addition(18,17)
+r2 = mathPackage.calculator.Subtraction(18,17)
+r3 = mathPackage.calculator.Product(18,17)
+r4 = mathPackage.calculator.Division(18,17)
