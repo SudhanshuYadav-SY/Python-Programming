53e671443f78e58768d1e4049aa88f36aa1dd672
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-08 13:36:17 +0530

    Add files via upload

--- Advance Python/Packages in Python.py
+++ Advance Python/Packages in Python.py
@@ -1,10 +1,16 @@
-from mathPackage.calculator import Addition
-from mathPackage.calculator import Subtraction
-from mathPackage.calculator import Product
-from mathPackage.calculator import Division
+from mathPackage.calculator import *
 
 
 r1 = Addition(18,17)
 r2 = Subtraction(18,17)
 r3 = Product(18,17)
 r4 = Division(18,17)
+
+print("--------------------------------------------------------------------------------------------------------------------------------------------")
+
+from animalPackage.Animals import Animals
+obj1 = Animals()
+obj1.Cat()
+obj1.Dog()
+obj1.Lion()
+obj1.Rabbit()
