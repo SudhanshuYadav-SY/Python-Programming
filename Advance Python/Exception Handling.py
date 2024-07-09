68c608ea51109b04aca7577b613dca58f8079190
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-09 13:10:48 +0530

    Add files via upload

---
+++ Advance Python/Exception Handling.py
@@ -0,0 +1,20 @@
+
+print("\n")
+
+print("Enter the value of Numerator")
+
+num = int(input())
+
+print("Enter the value of Denominator")
+
+den = int(input())
+
+try:
+
+    result = num/den
+
+    print(result)
+
+except:
+
+    print("Exception Handled Successfully!!")
