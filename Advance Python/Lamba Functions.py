2802110e0b5dec0bf9a58e09d01db1ec300b3e5a
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-10 12:10:57 +0530

    Add files via upload

--- Advance Python/Lamba Functions.py
+++ Advance Python/Lamba Functions.py
@@ -1,8 +1,22 @@
 
-sum1 = lambda A : A+10                              # Add A Certain Number and get the New Sum
+sum1 = lambda a : a+10                              # Add A Certain Number and get the New Sum
 
 print(sum1(10))
 
-sum = lambda a , b : a + b                          #Add 2 Random Numbers and get their Sum
+sum2 = lambda a , b : a + b                          #Add 2 Random Numbers and get their Sum
 
-print(sum(29,19))
+print(sum2(29,19))
+
+sum3 = lambda  a , b , c : a + b + c                 #Print Sum of 3 Random Numbers on Console
+
+print(sum3(10,5,5))
+
+prod1 = lambda a: a*12                              #Multiply any Number with 12 and get results
+
+print(prod1(19))
+
+prod2 = lambda a , b : a * b                        #Multiply any 2 Numbers and get results
+print(prod2(10,18))
+
+prod3 = lambda a , b , c : a * b * c                #Multiply any 3 Random Numbers and get results
+print(prod3(10,50,8))
