6dddea8f8f57e7071f8dd1d39d7ed43a0cabf004
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-10 12:49:07 +0530

    Add files via upload

--- Advance Python/Unpacking Collections.py
+++ Advance Python/Unpacking Collections.py
@@ -1,8 +1,8 @@
 
-#Case 1 Unpack Tuples
-print("Unpacking Tuples into Individual Variables")
+#Case 1 Unpack Sets
+print("Unpacking Sets into Individual Variables")
 
-a,b,c,d = (7,3,9,1)
+a,b,c,d = {7,3,9,1}
 
 print(a)
 
