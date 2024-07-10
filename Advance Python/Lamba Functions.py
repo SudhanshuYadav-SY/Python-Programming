02d5c68b3408edf1ad030f8823d6af50a8819b76
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-10 12:05:13 +0530

    Add files via upload

---
+++ Advance Python/Lamba Functions.py
@@ -0,0 +1,8 @@
+
+sum1 = lambda A : A+10                              # Add A Certain Number and get the New Sum
+
+print(sum1(10))
+
+sum = lambda a , b : a + b                          #Add 2 Random Numbers and get their Sum
+
+print(sum(29,19))
