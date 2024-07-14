dd1d1c1355a3837dacdeb38a4863680bf1ee24ac
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 12:29:28 +0530

    Add files via upload

--- Advance Python/Frozen Set.py
+++ Advance Python/Frozen Set.py
@@ -4,3 +4,7 @@ print(type(s))                                  #Print Data-Type of Set
 s.add(4)                                        #Add an element to Set
 print(s)                                        #Print Updated Set
 print(type(s))                                  #Print Data-Type of Set
+
+S = frozenset({1,9,6,8})                        #Demonstration of Frozen Set
+print(S)                                        #Print the Frozen Set
+print(type(S))                                  #Print Data-Type of Frozen Set
