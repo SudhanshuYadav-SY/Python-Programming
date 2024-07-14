3c99717acdc2631cde5a6a2f34e76de9b5b190a0
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 11:02:34 +0530

    Add files via upload

--- Advance Python/Complex Data Types.py
+++ Advance Python/Complex Data Types.py
@@ -1,6 +1,12 @@
 #Complex Data Types are Generally not used in real time scenarios but can learn it for knowledge purpose
 #They are generally used when we want to develop some scientific applications where complex mathematics is involved
+#Here j denotes the square root of -1
 
-c = 2 + 5j
-print(c)                                                    #Print the value of c
+c = 2 + 5j                                                  #Declaration of a Complex Number
+print(c)                                                    #Print the value of C
 print(type(c))                                              #Print Data-Type of C
+print(c.real)                                               #Print real-part of C
+print(c.imag)                                               #Print imaginary-part of C
+d = 6 + 13j                                                 #Declaration of Another Complex Number
+print(c+d)                                                  #Addition of 2 Complex Numbers
+print(d-c)                                                  #Subtract 2 Complex Numbers
