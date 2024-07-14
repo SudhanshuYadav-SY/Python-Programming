e88cfa6967c5aeae6fe3b0e273fafcef384b4240
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 13:48:49 +0530

    Add files via upload

--- Advance Python/Escape Charachters.py
+++ Advance Python/Escape Charachters.py
@@ -1,3 +1,3 @@
 #a = "My Name is \"Sudhanshu\" And I am learning \"Python\" Programming"
-a = "My Address is 3\4\5-3-2 Jagatpuri extension"
+a = "My Address is 3\\4\\5-3-2 Jagatpuri extension"
 print(a)
