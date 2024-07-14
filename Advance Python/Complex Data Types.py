f4cc62eee10db7fcd2bd29d9e0aea38a13ae3f92
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 10:55:25 +0530

    Add files via upload

---
+++ Advance Python/Complex Data Types.py
@@ -0,0 +1,5 @@
+#Complex Data Types are Generally not used in real time scenarios but can learn it for knowledge purpose
+#They are generally used when we want to develop some scientific applications where complex mathematics is involved
+
+c = 2 + 5j
+print(c)
