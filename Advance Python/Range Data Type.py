410dd9ff5d43f83fbdb56b652dd7dc0f6409c080
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 11:15:18 +0530

    Add files via upload

---
+++ Advance Python/Range Data Type.py
@@ -0,0 +1,13 @@
+print("Case 1 . . .")
+a = range(3)                                #Basic demonstration of numbers from 0 to 3
+print(a)                                    #Print Range from 0 to 3
+print(type(a))                              #Print data-type of the variable a
+
+print("Case 2 . . .")
+for v in a:                                 #For each value in the range print that variable v
+    print(v)
+
+b = range(2,10)                             #This will show numbers from 1 and end at 9
+print("Case 3 . . .")
+for c in b:
+    print(c)
