20196790027ad486dafe9bb8f5055d426787d71e
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 12:45:59 +0530

    Add files via upload

--- Advance Python/Generate Random Numbers.py
+++ Advance Python/Generate Random Numbers.py
@@ -1,3 +1,4 @@
 import random
 
 print(random.random())                  #Any random floating point value will be printed between 0 & 1
+print(random.random()*100)              #Any random floating point value will be printed between 0 & 100
