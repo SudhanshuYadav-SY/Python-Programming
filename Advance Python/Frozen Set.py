1917e45ff0d1176150f762b68dfedc7cac80f220
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 12:24:53 +0530

    Add files via upload

---
+++ Advance Python/Frozen Set.py
@@ -0,0 +1,6 @@
+s = {1,9,6,8}
+print(s)                                        #Print the Original Set
+print(type(s))                                  #Print Data-Type of Set
+s.add(4)                                        #Add an element to Set
+print(s)                                        #Print Updated Set
+print(type(s))                                  #Print Data-Type of Set
