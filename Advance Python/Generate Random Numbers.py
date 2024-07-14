95e4dc06a123fae5c13030ae6428e9cde262214d
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-14 12:53:46 +0530

    Add files via upload

--- Advance Python/Generate Random Numbers.py
+++ Advance Python/Generate Random Numbers.py
@@ -1,5 +1,9 @@
 import random
 
-print(random.random())                              #Any random floating point value will be printed between 0 & 1
-print(random.random()*100)                          #Any random floating point value will be printed between 0 & 100
-print(int(random.random()*100))                     #Any random int value will be printed between 1 & 100
+print(random.random())                                  #Any random floating point value will be printed between 0 & 1
+print(random.random()*100)                              #Any random floating point value will be printed between 0 & 100
+print(int(random.random()*100))                         #Any random int value will be printed between 1 & 100
+
+print(random.randint(1,20))                       #Print any random int value from 1 to 20 with both 1 and 20 included
+print(random.randint(1,100))                      #Print any random int value from 1 to 20 with both 1 and 100 included
+
