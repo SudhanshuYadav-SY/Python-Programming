3e91b54fddaccdf2584b078b672773572ad68720
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-15 12:44:28 +0530

    Add files via upload

---
+++ Advance Python/Iterator Function.py
@@ -0,0 +1,11 @@
+a = [8,18,7,891]
+
+itr = iter(a)                               #Iterate Each And Every Item in a by creating an iterator "itr"
+
+
+#Print each and every iterated element in a
+
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
