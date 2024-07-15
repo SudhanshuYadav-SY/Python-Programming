d99d97e6a4a04ec2ddbee72dc05d0379c819387e
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-15 12:33:16 +0530

    Add files via upload

--- Advance Python/Bool Function.py
+++ Advance Python/Bool Function.py
@@ -4,4 +4,6 @@ print(bool(1912))                                   #Will return TRUE
 print(bool(""))                                     #Will Return FALSE
 print(bool("Sudhanshu"))                            #Will Return TRUE
 print(bool([5,8,78,90]))                            #Will Return TRUE
+print(bool([]))                                     #Will Return FALSE
+print(bool((5,8,78,90)))                            #Will Return TRUE
 print(bool(()))                                     #Will Return FALSE
