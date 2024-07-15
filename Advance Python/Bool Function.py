777afdae8ab8c79391e92f6836eeeb88cb2218cd
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-15 12:20:06 +0530

    Add files via upload

--- Advance Python/Bool Function.py
+++ Advance Python/Bool Function.py
@@ -1,3 +1,4 @@
 print(bool(0))                             #Will return FALSE
 print(bool(1))                             #Will return TRUE
 print(bool(1912))                          #Will return TRUE
+print(bool(""))                            #Will Return False
