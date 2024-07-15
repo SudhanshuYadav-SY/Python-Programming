84d2b99bb904b03dfc9f381897b17b612ed58691
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-15 12:50:12 +0530

    Add files via upload

--- Advance Python/Iterator Function.py
+++ Advance Python/Iterator Function.py
@@ -1,11 +1,35 @@
-a = [8,18,7,891]
+a = [8,18,7,891]                            #Simple list
 
-itr = iter(a)                               #Iterate Each And Every Item in a by creating an iterator "itr"
+itr_A = iter(a)                               #Iterate Each And Every Item in a by creating an iterator "itr"
 
 
 #Print each and every iterated element in a
+print("Iterating Lists . . .")
+print(next(itr_A))
+print(next(itr_A))
+print(next(itr_A))
+print(next(itr_A))
 
-print(next(itr))
-print(next(itr))
-print(next(itr))
-print(next(itr))
+b = (8,18,7,891)
+
+itr_B = iter(b)                               #Iterate Each And Every Item in b by creating an iterator "itr"
+
+#Print each and every iterated element in b
+
+print("Iterating Tuple . . .")
+print(next(itr_B))
+print(next(itr_B))
+print(next(itr_B))
+print(next(itr_B))
+
+c = {8,18,7,891}
+
+itr_C = iter(c)                               #Iterate Each And Every Item in c by creating an iterator "itr"
+
+#Print each and every iterated element in c
+
+print("Iterating Sets . . .")
+print(next(itr_C))
+print(next(itr_C))
+print(next(itr_C))
+print(next(itr_C))
