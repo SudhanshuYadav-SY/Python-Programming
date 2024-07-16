5cecea6dc1b08a6f9c1b4c51c6ae4611384ae5e1
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-16 12:36:00 +0530

    Add files via upload

---
+++ Advance Python/Date & Time.py
@@ -0,0 +1,5 @@
+import datetime
+
+D = datetime.datetime(2024,7,16,12,33,40)               #Initialize Date and time and store in a variable
+
+print(D)                                                                                 #Print Date And Time
