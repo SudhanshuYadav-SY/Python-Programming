ed0daf62de3933a30891eed8fe48455a5267036b
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-16 13:18:54 +0530

    Add files via upload

--- Advance Python/Regular Expression.py
+++ Advance Python/Regular Expression.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = "[Pp]ython"               #Check if text starts with P or p then is the pattern and text matching or not
+pattern = "[Pp]ytho[nN]"               #Check if text starts with P or p then is the pattern and text matching or not
 print("Enter Sample Text")
 text = input()
 if re.search(pattern,text):
