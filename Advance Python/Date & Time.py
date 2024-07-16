9813a5e8257545996f79988236cf0dd60060312b
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-16 12:38:52 +0530

    Add files via upload

--- Advance Python/Date & Time.py
+++ Advance Python/Date & Time.py
@@ -3,3 +3,7 @@ import datetime
 D = datetime.datetime(2024,7,16,12,33,40)               #Initialize Date and time and store in a variable
 
 print(D)                                                                                 #Print Date And Time
+
+print(D.date())                                  #Print Only Date NOT time
+print(D.time())                                  #Print Only Time NOT Date
+print(D.day)                                     #Print Only Day
