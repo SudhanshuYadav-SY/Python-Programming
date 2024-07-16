7f46e4fbfdaa9be229f11b3c76fd5600cfa6ae86
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-16 13:29:31 +0530

    Add files via upload

--- Advance Python/Regular Expression.py
+++ Advance Python/Regular Expression.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = "[a-z]it"                              #Check if text starts with a certain pattern then is the pattern and text matching or not
+pattern = "[0-9]it"                              #Check if text starts with a certain pattern then is the pattern and text matching or not
 print("Enter Sample Text")
 text = input()
 if re.search(pattern,text):
