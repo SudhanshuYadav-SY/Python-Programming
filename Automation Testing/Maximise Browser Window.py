52be413f7077e62e61e2eb0e711e869816e7622a
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-17 23:38:07 +0530

    Add files via upload

---
+++ Automation Testing/Maximise Browser Window.py
@@ -0,0 +1,5 @@
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()                                         #Launch Chrome Browser
+driver.maximize_window()                                            #This Will Maximise Automation Window
