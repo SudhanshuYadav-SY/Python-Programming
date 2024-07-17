2f87bebfc061feefe3ffae9980b6f9053f2a768e
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-18 00:21:42 +0530

    Add files via upload

--- Automation Testing/Interaction With Web Elements.py
+++ Automation Testing/Interaction With Web Elements.py
@@ -2,12 +2,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()                                                                 #Launch Chrome Browser
-driver.maximize_window()                                                                    #This Will Maximise Automation Window
-driver.get("https://omayo.blogspot.com/")                                                   #Open Application URL
-driver.find_element(By.ID,"ta1").send_keys("Hello My Name is Sudhanshu")               #Identify Web Element using ID
-driver.find_element(By.NAME,"q").send_keys("Automation Testing")                       #Identify Web Element using NAME
-driver.find_element(By.CLASS_NAME,"dropbtn").click()                                   #Identify Web Element using CLASS NAME
-driver.find_element(By.LINK_TEXT,"compendiumdev").click()                              #Identify Web Element Using LINK TEXT
-time.sleep(4)                                                                                #Wait for 4 seconds on web page
-driver.quit()                                                                                #Close Automation Window
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+driver.find_element(By.ID,"ta1").send_keys("Hello My Name is Sudhanshu")                    #Identify Web Element using ID
+driver.find_element(By.NAME,"q").send_keys("Automation Testing")                            #Identify Web Element using NAME
+driver.find_element(By.CLASS_NAME,"dropbtn").click()                                        #Identify Web Element using CLASS NAME
+driver.find_element(By.XPATH,"//*[@value='Login']").click()                                 #Identify Web Element Using XPATH
+driver.find_element(By.LINK_TEXT,"compendiumdev").click()                                   #Identify Web Element Using LINK TEXT
+driver.find_element(By.CSS_SELECTOR,"input[value='Login']").click()                         #Identify Web Element Using CSS SELECTOR
+time.sleep(4)                                                                                     #Wait for 4 seconds on web page
+driver.quit()                                                                                     #Close Automation Window
