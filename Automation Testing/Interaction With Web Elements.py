c0002ee30d45d7a9b5715f7862cf714c62b1ae04
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-18 00:12:28 +0530

    Add files via upload

--- Automation Testing/Interaction With Web Elements.py
+++ Automation Testing/Interaction With Web Elements.py
@@ -8,5 +8,6 @@ driver.get("https://omayo.blogspot.com/")                                       
 driver.find_element(By.ID,"ta1").send_keys("Hello My Name is Sudhanshu")               #Identify Web Element using ID
 driver.find_element(By.NAME,"q").send_keys("Automation Testing")                       #Identify Web Element using NAME
 driver.find_element(By.CLASS_NAME,"dropbtn").click()                                   #Identify Web Element using CLASS NAME
+driver.find_element(By.LINK_TEXT,"compendiumdev").click()                              #Identify Web Element Using LINK TEXT
 time.sleep(4)                                                                                #Wait for 4 seconds on web page
 driver.quit()                                                                                #Close Automation Window
