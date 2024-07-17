508579d821f58dea0e89c15c3068c17cff69a531
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-18 00:30:45 +0530

    Add files via upload

---
+++ Automation Testing/Click Different Web Elements.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+driver.find_element(By.ID,"alert1").click()                                                 #Web Element Clicked
+time.sleep(5)
+driver.quit()
