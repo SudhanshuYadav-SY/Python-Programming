7f9f4b5ccafb731e720c91241d20fdaa91d13101
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-18 20:24:30 +0530

    Add files via upload

---
+++ Automation Testing/Clear Text Field.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+
+#Let's first clear Text from text box
+
+driver.find_element(By.ID,"textbox1").clear()                                               #Clear Pre-loaded text from text area
+
+time.sleep(5)
