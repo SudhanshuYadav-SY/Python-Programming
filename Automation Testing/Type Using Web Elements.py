5416304927eaf0762ab610387d96818e9f8cfb7e
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-18 18:10:17 +0530

    Add files via upload

--- Automation Testing/Type Using Web Elements.py
+++ Automation Testing/Type Using Web Elements.py
@@ -6,6 +6,9 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
 driver.maximize_window()                                                                          #This Will Maximise Automation Window
 driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
-driver.find_element(By.ID,"ta1").send_keys("My Name is Sudhanshu \n\n\n\n\n And Currently Learning Python")     #Interact with Web Element
+driver.find_element(By.ID,"ta1").send_keys("My Name is Sudhanshu \n\n\n\n\n And Currently Learning Python")     #Type Text inside that Text Area
 time.sleep(5)
+driver.find_element(By.NAME,"q").send_keys("Automation Testing")                #Type Text in Text Box
+time.sleep(5)
+
 driver.quit()
