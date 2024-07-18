7e4df6fc1d645f3a82313b55cbc1260e62c2c523
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-18 18:26:43 +0530

    Add files via upload

---
+++ Automation Testing/Multiple Actions On Web Element.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+driver.find_element(By.NAME,"fname").clear()                                                #Clear That Text Field
+time.sleep(2)
+driver.quit()
