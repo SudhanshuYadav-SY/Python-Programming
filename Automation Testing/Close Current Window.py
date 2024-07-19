b35aa000ce6308b4ba215822e6574beac11c4b58
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-19 17:38:32 +0530

    Add files via upload

---
+++ Automation Testing/Close Current Window.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+
+time.sleep(5)
+
+driver.close()
