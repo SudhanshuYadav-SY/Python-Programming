26db74e42e1bb56ad1d7258f9994cab8e7157d39
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-19 13:33:53 +0530

    Add files via upload

--- Automation Testing/Get URL of Web Page.py
+++ Automation Testing/Get URL of Web Page.py
@@ -12,6 +12,20 @@ driver.get("https://omayo.blogspot.com/")                                       
 
 url_1 = driver.current_url
 
+print("\n\n")
+
 print("Current URL is - "+url_1)
+
+time.sleep(5)
+
+driver.find_element(By.LINK_TEXT,"onlytestingblog").click()
+
+url_2 = driver.current_url
+
+time.sleep(5)
+
+print("\n\n")
+
+print("Now the URL is - "+url_2)
 
 driver.quit()
