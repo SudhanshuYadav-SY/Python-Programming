12b21fb87539e460d741c4131f1451678bfec23a
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-19 13:20:46 +0530

    Add files via upload

---
+++ Automation Testing/Get URL of Web Page.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+
+url_1 = driver.current_url
+
+print("Current URL is - "+url_1)
+
+driver.quit()
