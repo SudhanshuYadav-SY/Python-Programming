2bf86795c10ec4966b16d562bb46a69db97e1d32
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-21 18:13:08 +0530

    Add files via upload

---
+++ Automation Testing/Size of Web Element.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://omayo.blogspot.com/")                                                        #Open Application URL
+
+time.sleep(5)
+
+WebElement_Size = driver.find_element(By.ID,"ta1").size
+
+print(WebElement_Size)
+
+driver.quit()
