5fddd7c09d82e92f9e48b75ee1767fc11bcbf30c
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-21 13:41:37 +0530

    Add files via upload

--- Automation Testing/Get HTML Code.py
+++ Automation Testing/Get HTML Code.py
@@ -1,5 +1,5 @@
 import time
-
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
 from selenium.webdriver.common.by import By
@@ -14,4 +14,10 @@ time.sleep(5)
 
 HTML_Code = driver.page_source                                                                  #Command to get HTML Source Code
 
-print("HTML Code for the page - \n"+HTML_Code)
+soup = BeautifulSoup(HTML_Code,'html.parser')                                           #Use Beautiful Soup to Parse the HTML Code
+
+pretty_html = soup.prettify()                                                                   #Format the respective HTML Code in a pretty way
+
+print("Non- Pretty HTML Code for the page - \n"+HTML_Code)                                      #Print Non-Pretty HTML Code
+
+print("Pretty HTML Code for the page is - \n"+pretty_html)                                      #Print the Pretty HTML Code
