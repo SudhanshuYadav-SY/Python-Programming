150c21d83a3d9e7c7c47107f286514f6c3f27e93
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-21 13:44:02 +0530

    Add files via upload

---
+++ Automation Testing/WebPage in Full Screen.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://omayo.blogspot.com/")                                                         #Open Application URL
+
+time.sleep(5)
+
+driver.fullscreen_window()                                                                      #View Web Page into Full Screen
+
+time.sleep(5)
+
+driver.quit()
