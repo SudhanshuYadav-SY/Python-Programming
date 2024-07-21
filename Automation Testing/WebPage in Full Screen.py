f0fe0bac498908850516999a6093d0d1c407eaa4
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-21 13:57:50 +0530

    Add files via upload

--- Automation Testing/WebPage in Full Screen.py
+++ Automation Testing/WebPage in Full Screen.py
@@ -2,6 +2,8 @@ import time
 
 from selenium import webdriver
 
+from selenium.webdriver import Keys
+
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
@@ -12,7 +14,17 @@ driver.get("https://omayo.blogspot.com/")                                       
 
 time.sleep(5)
 
+driver.find_element(By.ID,"ta1").send_keys("Currently You are in maximise mode")
+
 driver.fullscreen_window()                                                                      #View Web Page into Full Screen
+
+time.sleep(5)
+
+driver.find_element(By.ID,"ta1").clear()
+
+time.sleep(4)
+
+driver.find_element(By.ID,"ta1").send_keys("You are in Full Screen Now")
 
 time.sleep(5)
 
