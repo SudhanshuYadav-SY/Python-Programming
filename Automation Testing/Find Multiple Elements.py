836651c0fb3c876cf8054f6bdb097bf10b791301
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-23 18:56:27 +0530

    Add files via upload

---
+++ Automation Testing/Find Multiple Elements.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://omayo.blogspot.com/")
+
+options = driver.find_elements(By.XPATH,"//select[@id='multiselect1']/option")
+
+print("Your Options are - ")
+
+for i in options:
+    print(i.text)
+
+driver.quit()
