d94c639d8552211a75be7d90f2147f86dfa9eebc
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-24 13:37:08 +0530

    Add files via upload

---
+++ Automation Testing/Handle Javascript Information Alerts.py
@@ -0,0 +1,42 @@
+import time
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                                                                       #Launch Chrome Browser
+
+driver.maximize_window()                                                                          #This Will Maximise Automation Window
+
+driver.get("https://the-internet.herokuapp.com/")                                                 #Open Application URL
+
+driver.find_element(By.LINK_TEXT,"JavaScript Alerts").click()
+
+time.sleep(5)
+
+driver.find_element(By.XPATH,"//*[text()='Click for JS Confirm']").click()
+
+time.sleep(5)
+
+#This code will generate error as we did not handle previous exception
+"""driver.find_element(By.XPATH,"//*[text()='Click for JS Confirm']").click()
+
+time.sleep(5)
+"""
+#Now lets handle this alert which pooped out
+
+info_alert = driver.switch_to.alert
+
+print("Alert Text is -\n"+info_alert.text)
+
+info_alert.dismiss()
+
+print("Result is -\n"+driver.find_element(By.ID,"result").text)
+
+time.sleep(5)
+
+driver.find_element(By.LINK_TEXT,"Elemental Selenium").click()
+
+time.sleep(5)
+
+driver.quit()
